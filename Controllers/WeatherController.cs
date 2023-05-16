566d8b4e4df522466aba2068f4609729a1b020df
malikhuremovic01 <malikhuremovic01@gmail.com>
2023-05-16 08:57:25 +0200

    Registered controller with three GET endpoints

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Services;
+
+namespace WeatherAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherService _weatherService;
+        public WeatherController(IWeatherService weatherService) {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<string>> GetCurrentWeather(string location)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<string>> GetWeatherForecast(string location)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<string>> GetWeatherHistory(string location)
+        {
+            return null;
+        }
+
+        /*
+         /weather/current: returns the current weather conditions for a specific location
+/weather/forecast: returns the weather forecast for a specific location
+/weather/history: returns historical weather data for a specific location
+         */
+    }
+}
