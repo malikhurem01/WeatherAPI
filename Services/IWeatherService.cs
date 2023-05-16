ce7d62c3352be12bbf5ef2dc545c60249b5e10bb
malikhuremovic01 <malikhuremovic01@gmail.com>
2023-05-16 08:57:38 +0200

    Registered service with IService interface

---
+++ Services/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherAPI.Services
+{
+    public interface IWeatherService
+    {
+        Task<string> GetCurrentWeather(string location);
+        Task<string> GetWeatherForecast(string location);
+        Task<string> GetWeatherHistory(string location);
+    }
+}
---
+++ Services/WeatherService.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherAPI.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        public Task<string> GetCurrentWeather(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetWeatherForecast(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetWeatherHistory(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
