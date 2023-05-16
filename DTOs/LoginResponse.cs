39fb67f6b2dbb97b1b1041e13d3ef2a1063251e1
malikhuremovic01 <malikhuremovic01@gmail.com>
2023-05-16 13:46:11 +0200

    Added data transfer objects for User entity

---
+++ DTOs/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherAPI.DTOs
+{
+    public class LoginResponse
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
