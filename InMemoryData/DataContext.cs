9a51dfd321a9b4a1c2077af53fc7c0b0c5aede7e
malikhuremovic01 <malikhuremovic01@gmail.com>
2023-05-16 13:48:13 +0200

    In memory database configured

---
+++ InMemoryData/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.InMemoryData
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "UsersDb");
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
