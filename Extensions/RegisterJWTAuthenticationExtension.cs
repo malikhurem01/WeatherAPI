fb9aef5f37879767412123b4a25f646c00554a8b
malikhuremovic01 <malikhuremovic01@gmail.com>
2023-05-16 13:46:41 +0200

    Added extension for services registration

---
+++ Extensions/RegisterJWTAuthenticationExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WeatherAPI.Extensions
+{
+    public static class RegisterJWTAuthenticationExtension
+    {
+        public static void RegisterJWTAuthentication(this IServiceCollection service, ConfigurationManager config)
+        {
+            service.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(config.GetSection("Authentication:SecurityKey").Value))
+                };
+            });
+        }
+    }
+}
